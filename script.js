90220bceb94826f70e38e3e9ec6a287a30c1b8a6
JuditBarabas <62601033+JuditBarabas@users.noreply.github.com>
2020-10-24 23:29:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log('For solution open js file');
+
+function getColor(phrase){
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+   if (phrase === 'stop') {
+       console.log('red');
+   } else if(phrase === 'slow') {
+       console.log('yellow');
+   } else if(phrase === 'go') {
+       console.log('green');
+   } else {
+       console.log('purple');
+   }
+   
+    //AND THIS LINE ↑↑↑↑↑
+}
